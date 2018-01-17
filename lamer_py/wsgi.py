ac7476c44b9e2bdea13e00c8616de50abc2b3af2
ubltroll <chen1045bc@vip.qq.com>
2018-01-17 20:41:52 +0800

    0.2.3

--- lamer_py/wsgi.py
+++ lamer_py/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+from os.path import join,dirname,abspath
 import sys
 from django.core.wsgi import get_wsgi_application
 
