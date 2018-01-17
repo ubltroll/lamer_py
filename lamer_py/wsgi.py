42330325debcacb34aa7aa220f5a38259c48b7df
ubltroll <chen1045bc@vip.qq.com>
2018-01-17 20:36:27 +0800

    0.2.0
    for apache

--- lamer_py/wsgi.py
+++ lamer_py/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lamer_py.settings")
 
+sys.path.insert(0,PROJECT_DIR)
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
 application = get_wsgi_application()
